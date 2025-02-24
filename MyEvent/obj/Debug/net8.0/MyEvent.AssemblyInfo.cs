487ac907290dc004ed34a1e2749f2e3a7bc6260b
kkkkaon <bigbrain1996@gmail.com>
2025-02-24 16:24:01 +0800

    update

--- MyEvent/obj/Debug/net8.0/MyEvent.AssemblyInfo.cs
+++ MyEvent/obj/Debug/net8.0/MyEvent.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyEvent")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6b3836292425b491d199f20f79aa1c7b2708532")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bebcbcf6f06d0616609fd7662172e570996c1a02")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyEvent")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyEvent")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
